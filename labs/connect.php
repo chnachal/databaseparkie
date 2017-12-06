46c58db7e1b0c1bad282a438df81035e6bf90211
chnachal <34313153+chnachal@users.noreply.github.com>
2017-12-06 22:36:33 +0530

    Update connect.php
    update use and pass

--- labs/connect.php
+++ labs/connect.php
@@ -1,6 +1,6 @@
 
 <?php 
-$mysqli = new mysqli("eu-cdbr-azure-west-c.cloudapp.net", "be4c54a5038438", "dd408c47", "mystudents");
+$mysqli = new mysqli("parkieserver.mysql.database.azure.com", "parkieser@parkieserver", "americanUSA1", "parkieserver");
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
