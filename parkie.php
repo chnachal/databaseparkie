6c308f079c5be40db0b80bc33998c5fa461f0553
chnachal <34313153+chnachal@users.noreply.github.com>
2017-12-06 23:09:13 +0530

    Create parkie.php

---
+++ parkie.php
@@ -0,0 +1,8 @@
+$connection = mysqli_connect("parkieserver.mysql.database.azure.com", "parkieser@parkieserver", "americaUSA1", "parkiedb");
+$deviceid = $_POST['deviceis']; $statusID = $_POST['statusID']; 
+$sql = "INSERT INTO parkiedb.parkie (deviceid, statusID) VALUES ('20171201', 'NotAvail')";
+if(mysqli_query($connection, $sql)){
+    echo "Records inserted successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($connection);
+}
