06cf39c455a86a0d45dfa2735415292f3ea60d25
chnachal <34313153+chnachal@users.noreply.github.com>
2017-12-07 07:57:43 +0530

    Add files via upload

---
+++ t.php
@@ -0,0 +1,10 @@
+<?php 
+$connection = mysqli_connect("parkieserver.mysql.database.azure.com", "parkieser@parkieserver", "americaUSA1", "parkieserver");
+$deviceid = $_POST['deviceis']; $statusID = $_POST['statusID']; 
+$sql = "INSERT INTO parkiedb.parkiet (deviceid, statusID) VALUES ('20171201', 'NotAvail')";
+if(mysqli_query($connection, $sql)){
+    echo "Records inserted successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($connection);
+}
+?>
