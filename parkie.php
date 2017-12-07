8fe52e9a7d72f45483a3926b30da0dce919bd843
chnachal <34313153+chnachal@users.noreply.github.com>
2017-12-07 08:11:19 +0530

    start

--- parkie.php
+++ parkie.php
@@ -1,6 +1,6 @@
 $connection = mysqli_connect("parkieserver.mysql.database.azure.com", "parkieser@parkieserver", "americaUSA1", "parkiedb");
 $deviceid = $_POST['deviceis']; $statusID = $_POST['statusID']; 
-$sql = "INSERT INTO parkiedb.parkie (deviceid, statusID) VALUES ('20171201', 'NotAvail')";
+$sql = "INSERT INTO parkiedb.parkie1 (deviceid, statusID) VALUES ('20171201', 'NotAvail')";
 if(mysqli_query($connection, $sql)){
     echo "Records inserted successfully.";
 } else{
