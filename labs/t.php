cac1f9109463c384d26e3b487b21b74b54648381
chnachal <34313153+chnachal@users.noreply.github.com>
2017-12-07 07:52:41 +0530

    Update t.php

--- labs/t.php
+++ labs/t.php
@@ -1,5 +1,5 @@
 <?php 
-$connection = mysqli_connect("parkieserver.mysql.database.azure.com", "parkieser@parkieserver", "americaUSA1", "parkiedb");
+$connection = mysqli_connect("parkieserver.mysql.database.azure.com", "parkieser@parkieserver", "americaUSA1", "parkieserver");
 $deviceid = $_POST['deviceis']; $statusID = $_POST['statusID']; 
 $sql = "INSERT INTO parkiedb.parkie (deviceid, statusID) VALUES ('20171201', 'NotAvail')";
 if(mysqli_query($connection, $sql)){
