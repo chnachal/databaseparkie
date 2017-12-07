2a123f8156142313be06983e9340235b21e75901
chnachal <34313153+chnachal@users.noreply.github.com>
2017-12-07 09:16:19 +0530

    Update connect.php

--- labs/connect.php
+++ labs/connect.php
@@ -4,6 +4,6 @@ $mysqli = new mysqli("parkieserver.mysql.database.azure.com", "parkieser@parkies
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
-//echo $mysqli->host_info . "\n";
+echo $mysqli->host_info . "\n";
 
 ?>
